5e97e0bda69adf8a99af01e66e165f5ea2d7da4c
nhvinh118 <nguyen_h_vinh@yahoo.com>
2019-02-21 21:45:58 -0500

    gitflow

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
-files = []
-files = open('vntxt.txt','w')
-for x in range(100):
-    files.writelines(x)
+# files = []
+# files = open('vntxt.txt','w')
+# for x in range(100):
+#     files.writelines(x)
     
-files.close
+# files.close
+
+with open('vntxt.txt','w') as f:
+    for x in range('a':'Z'):
+        files.writelines(x)  
